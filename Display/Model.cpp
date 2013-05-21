1f6b10ec81fcc24d2e5d1abd6d255ee930221661
helain_f <helain_f@helain-f-HP-EliteBook-8540p.(none)>
2013-05-21 13:51:44 +0200

    Update

--- Display/Model.cpp
+++ Display/Model.cpp
@@ -5,7 +5,7 @@
 // Login   <helain_f@epitech.net>
 // 
 // Started on  Tue May 21 11:45:55 2013 Helaine
-// Last update Tue May 21 12:07:10 2013 Helaine
+// Last update Tue May 21 13:46:21 2013 Helaine
 //
 
 #include	"Model.hpp"
@@ -17,10 +17,10 @@ namespace	Model
     this->model_ = gdl::Model::load("../libgdl_gl-2012.4/assets/models/marvin.fbx");
   }
 
-  Bomberman::~Bomberman(void)
+  /*Bomberman::~Bomberman(void)
   {
     //delete this->model_;
-  }
+    }*/
 
   void Bomberman::update(gdl::GameClock const & gameClock, gdl::Input & input)
   {
